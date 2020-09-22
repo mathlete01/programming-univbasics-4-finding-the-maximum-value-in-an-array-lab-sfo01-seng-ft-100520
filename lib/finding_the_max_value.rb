18923e64ed2a203af3a3287a2045d283c761ba9d
Matthew Sallin <msallin@gmail.com>
2020-09-22 20:50:34 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
 def find_max_value(array)
-  # Add your solution here
+  counter = 0
+  candidate = 0
+  while counter < array.length do
+    if array[counter] > candidate
+      candidate = array[counter]
+    end
+    counter += 1
+  end
+  return candidate
 end
